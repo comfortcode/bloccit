dbf65c65e6cf249051e497427b377eb857ccd95a
Nechama Goldman <nechamabgoldman@gmail.com>
2015-06-03 23:08:23 +0000

    Seed Data Assignment complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@
      body:   Faker::Lorem.paragraph
    )
  end
+
  posts = Post.all
  
  # Create Comments
@@ -16,7 +17,17 @@
      body: Faker::Lorem.paragraph
    )
  end
- 
+
+ Post.create!(
+  title: "New Manual Post",
+  body: "blah blah blah blaaah"
+)
+
+Comment.create!(
+  post: posts.sample, #How would I specify a specific post?
+  body: "Comment I put in manually"
+)
+
  puts "Seed finished"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
