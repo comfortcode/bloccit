b63dce83a9a159f90818377357f67963ab355f7e
Nechama Goldman <nechamabgoldman@gmail.com>
2015-06-25 03:26:50 +0000

    Auth Cont' Assignment completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,12 @@
 class PostsController < ApplicationController
+  
+# before_action :flash_attack
+skip_before_action :flash_attack, only: [:index, :new]
+  
+  def flash_attack
+    flash[:notice] = "Flash Attack"
+  end 
+  
   def index
     @posts = Post.all
   end
