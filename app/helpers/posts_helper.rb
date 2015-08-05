5896de4179a748d9ea844b32cb0f20dfe945c0b8
Nechama Goldman <nechamabgoldman@gmail.com>
2015-08-05 15:21:55 +0000

    Markdown Assignment completed

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,17 @@
 module PostsHelper
+  
+  def markdown_title(post)
+    render_as_markdown(post.title)
+  end 
+
+  def markdown_body(post)
+    render_as_markdown(post.body)
+  end 
+  
+  def render_as_markdown(text)
+    renderer = Redcarpet::Render::HTML.new
+    extensions = {fenced_code_blocks: true}
+    redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+    (redcarpet.render text).html_safe
+  end 
 end
