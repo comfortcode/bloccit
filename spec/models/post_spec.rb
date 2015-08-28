d2d6f521524a7e79b46fc04e783f1cb6887bef66
Nechama Goldman <nechamabgoldman@gmail.com>
2015-08-28 19:38:28 +0000

    Voting Checkpoint first commit

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,7 +4,7 @@
    describe "vote methods" do
  
      before do
-       @post = Post.create(title: 'post title', body: 'post body')
+       @post = Post.create(title: 'post title', body: 'post body is longer now, so it should work just fine.')
        3.times { @post.votes.create(value: 1) }
        2.times { @post.votes.create(value: -1) }
      end
