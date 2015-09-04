0de245ea95306c76d7c02bead2e6046cf00b07f1
Nechama Goldman <nechamabgoldman@gmail.com>
2015-09-04 18:03:02 +0000

    Favoriting Checkpoint completed

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,13 +3,31 @@ class FavoritesController < ApplicationController
   def create
      post = Post.find(params[:post_id])
      favorite = current_user.favorites.build(post: post)
+     authorize favorite
  
      if favorite.save
        flash[:notice] = "You have favorited the post."
        redirect_to [post.topic, post]
      else
-       flash[:error] = "There was an error favoriting the comment. Please try again."
+       flash[:error] = "There was an error favoriting the post. Please try again."
        render :new
      end
    end
+  
+def destroy
+     post = Post.find(params[:post_id])
+     user = current_user
+     favorite = Favorite.where(post: post, user: user).first
+     authorize favorite 
+  
+   if favorite.destroy
+     flash[:notice] = "You have unfavorited this post"
+     redirect_to [post.topic, post]
+   else
+       flash[:error] = "There was an error unfavoriting the post. Please try again."
+       render :new
+   end
+     
+ end
+
 end
