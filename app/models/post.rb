0147a0f1a7ecb36f11f087b2c5c22e53ccdce093
Nechama Goldman <nechamabgoldman@gmail.com>
2015-09-04 16:30:22 +0000

    Mocking Assignment completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,27 +12,32 @@ class Post < ActiveRecord::Base
    validates :topic, presence: true
    validates :user, presence: true
 
+ def save_with_initial_vote
+   ActiveRecord::Base.transaction do
+      save
+      create_vote
+    end 
+ end 
+
   def create_vote
     user.votes.create(value:1, post_id: id)
   end
   
-  def up_votes
+ def up_votes
      votes.where(value: 1).count
-   end 
+ end 
 
   def down_votes
      votes.where(value: -1).count
-   end   
+  end   
   
   def points
     votes.sum(:value)
-   end   
+  end   
   
   def update_rank
      age_in_days = (created_at - Time.new(1970,1,1)) / (60 * 60 * 24) # 1 day in seconds
-     new_rank = points + age_in_days
- 
+     new_rank = points + age_in_days 
      update_attribute(:rank, new_rank)
-   end
-
+  end
 end
